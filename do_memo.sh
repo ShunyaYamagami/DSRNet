a065d67c43b28837d03fc237feb5cdc9c77c7a0c
ShunyaYamagami <othello200063@gmail.com>
2024-06-11 13:17:57 +0900

    nits

--- do_memo.sh
+++ do_memo.sh
@@ -1,4 +1,4 @@
-python test_me.py \
+poetry run python test_me.py \
     --inet dsrnet_s \
     --model dsrnet_model_sirs \
     --dataset sirs_dataset  \
@@ -7,5 +7,18 @@ python test_me.py \
     --if_align \
     --resume \
     --weight_path "./weights/dsrnet_s_epoch14.pt" \
-    --base_dir "comp/"
+    --base_dir "patches_bad/"
+
+
+
+# 
+poetry run python -m src.classification.classification_patches_with_refrem \
+    --inet dsrnet_s \
+    --model dsrnet_model_sirs \
+    --dataset sirs_dataset  \
+    --name dsrnet_s_test \
+    --hyper \
+    --if_align \
+    --resume \
+    --weight_path "./DSRNet/weights/dsrnet_s_epoch14.pt" 
 
